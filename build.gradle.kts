8fb2aba67c5837ae75571c1409e16e416bbd2b7f
emortal <nathanpenwill88@gmail.com>
2022-03-29 22:48:37 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin.
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
-    // Kotlinx serialization for any data format
-    kotlin("plugin.serialization") version "1.6.10"
-    // Shade the plugin
+    //kotlin("plugin.serialization") version "1.6.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    // Allow publishing
     `maven-publish`
 
-    // Apply the application plugin to add support for building a jar
     java
-    // Dokka documentation w/ kotlin
-    id("org.jetbrains.dokka") version "1.6.10"
 }
 
 repositories {
@@ -23,52 +16,28 @@ repositories {
 
     maven(url = "https://jitpack.io")
     maven(url = "https://repo.spongepowered.org/maven")
-    maven(url = "https://repo.minestom.com/repository/maven-public/")
-    maven(url = "https://repo.velocitypowered.com/snapshots/")
 }
 
 dependencies {
-    // Align versions of all Kotlin components
-    compileOnly(platform("org.jetbrains.kotlin:kotlin-bom"))
+    //compileOnly(kotlin("stdlib"))
 
-    // Use the Kotlin JDK 8 standard library.
-    compileOnly(kotlin("stdlib"))
-
-    // Use the Kotlin reflect library.
-    compileOnly(kotlin("reflect"))
-
-    // Compile Minestom into project
     compileOnly("com.github.Minestom:Minestom:338ffb80db")
-
-    // import kotlinx serialization
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    //compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 }
 
-configurations {
-    testImplementation {
-        extendsFrom(configurations.compileOnly.get())
-    }
-}
-
-// Take gradle.properties and apply it to resources.
 tasks {
     processResources {
-        // Apply properties to extension.json
         filesMatching("extension.json") {
             expand(project.properties)
         }
     }
 
-    // Set name, minimize, and merge service files
     named<ShadowJar>("shadowJar") {
         archiveBaseName.set(project.name)
         mergeServiceFiles()
         minimize()
     }
 
-    test { useJUnitPlatform() }
-
-    // Make build depend on shadowJar as shading dependencies will most likely be required.
     build { dependsOn(shadowJar) }
 
 }
